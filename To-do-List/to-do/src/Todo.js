7463ad4dbf3bf1cf3968952e18031c2424282290
gokul-goklu <gokulshanmugam062@gmail.com>
2023-07-16 13:42:08 +0530

    sample commit

---
+++ To-do-List/to-do/src/Todo.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+export const Todo = () => {
+  const [changeTodo, setchangeTodo] = useState([]);
+  const [Submitchange, setSubmitchange] = useState("");
+
+  const textToDo = (e) => {
+    setSubmitchange(e.target.value);
+  };
+
+  const submitText = () => {
+    setchangeTodo(...changeTodo, Submitchange);
+    setSubmitchange("");
+  };
+
+  return (
+    <div>
+      <input type="text" onChange={textToDo} />
+      <button onClick={submitText}>Submit</button>
+      {changeTodo.map((changeTodos) => (
+        <div>{changeTodos}</div>
+      ))}
+    </div>
+  );
+};
